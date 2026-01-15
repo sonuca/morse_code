2546e18fc20254575c7bfacc9ed4a20733046a2e
sonuca <sonaramlova@gmail.com>
2026-01-15 13:11:19 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+	"log"
+	"os"
+)
 
+func main() {
+	// созданеи логгера
+	logger := log.New(os.Stdout, "INFO: ", log.LstdFlags)
+	// создание сервера
+	srv := server.NewServer(logger)
+	logger.Fatal(srv.HTTP.ListenAndServe())
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,67 @@
 package service
+
+import (
+	"strings"
+)
+
+var textMorseCode = map[string]string{
+	"А": ".-", "Б": "-...", "В": ".--", "Г": "--.", "Д": "-..", "Е": ".",
+	"Ж": "...-", "З": "--..", "И": "..", "Й": ".---", "К": "-.-", "Л": ".-..",
+	"М": "--", "Н": "-.", "О": "---", "П": ".--.", "Р": ".-.", "С": "...",
+	"Т": "-", "У": "..-", "Ф": "..-.", "Х": "....", "Ц": "-.-.", "Ч": "---.",
+	"Ш": "----", "Щ": "--.-", "Ъ": ".--.-.", "Ы": "-.--", "Ь": "-..-", "Э": "..-..",
+	"Ю": "..--", "Я": ".-.-", "1": ".----", "2": "..---", "3": "...--", "4": "....-",
+	"5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.", "0": "-----", " ": "/",
+}
+
+var morseCodeText = func() map[string]string {
+	m := make(map[string]string)
+	for key, value := range textMorseCode {
+		m[value] = key
+	}
+	return m
+}()
+
+func Converter(input string) string {
+	input = strings.TrimSpace(input)
+	if input == "" {
+		return ""
+	}
+
+	// определяем: это Морзе или текст
+	isMorse := true
+	for _, sign := range input {
+		if sign != '.' && sign != '-' && sign != ' ' && sign != '/' {
+			isMorse = false
+			break
+		}
+	}
+
+	if isMorse {
+		// декодируем Морзе в текст
+		parts := strings.Split(input, " ")
+		var decoded []string
+		for _, code := range parts {
+			if code == "" {
+				continue
+			}
+			if char, ok := morseCodeText[code]; ok {
+				decoded = append(decoded, char)
+			} else {
+				return ""
+			}
+		}
+		return strings.Join(decoded, "")
+	}
+	// кодируем текст в Морзе
+	input = strings.ToUpper(input)
+	var encoded []string
+	for _, r := range input {
+		if code, ok := textMorseCode[string(r)]; ok {
+			encoded = append(encoded, code)
+		} else {
+			return ""
+		}
+	}
+	return strings.Join(encoded, " ")
+}
